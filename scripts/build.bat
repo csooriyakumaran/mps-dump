da22f53a88221df61f4220580e6e084f5ca9c1b4
csooriyakumaran <c.sooriyakumaran@gmail.com>
2026-04-04 21:07:23 -0400

    updated build.bat for optimized release builds

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,2 @@
-cmake -S . -B build -G "Ninja" -D CMAKE_C_COMPILER=clang
+cmake -S . -B build -G "Ninja" -D CMAKE_C_COMPILER=clang -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_FLAGS_RELEASE="-O3 -g0 -DNDEBUG"
 cmake --build build
